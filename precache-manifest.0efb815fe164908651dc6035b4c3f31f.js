f05e1c5eb097f0170792720d8a9b9bbb900ad0ff
codeFreeman <Freemanartstudio@hotmail.com>
2019-10-11 14:34:39 +0800

    Updates

---
+++ precache-manifest.0efb815fe164908651dc6035b4c3f31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f091282088d4e61832caf211b3d4552a",
+    "url": "/codeFreeman/index.html"
+  },
+  {
+    "revision": "641425c0d7f745250da1",
+    "url": "/codeFreeman/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "df549a8fb8b440340f3e",
+    "url": "/codeFreeman/static/js/2.31f72f36.chunk.js"
+  },
+  {
+    "revision": "641425c0d7f745250da1",
+    "url": "/codeFreeman/static/js/main.7a51d023.chunk.js"
+  },
+  {
+    "revision": "4b35d5ee73a5f618d819",
+    "url": "/codeFreeman/static/js/runtime-main.6732dbad.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/codeFreeman/static/media/logo.25bf045c.svg"
+  }
+]);
