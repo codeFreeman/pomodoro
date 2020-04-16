3ecd6059a5e1910fabc4d5efe07ce24f372668ff
codeFreeman_dp <chyang@dreamplayer.com.tw>
2020-04-16 17:59:09 +0800

    add konva& spring

--- src/components/view/timer/counttomato/RoundTomato.js
+++ src/components/view/timer/counttomato/RoundTomato.js
@@ -1,59 +1,34 @@
 import React, { useState, useRef } from "react";
+import { Box } from "@material-ui/core";
 import Konva from "konva";
-import { Stage, Layer, Shape } from "react-konva";
-import { Spring, animated } from "react-spring/renderprops-konva";
+import { Stage, Layer, Shape, Circle } from "react-konva";
+import { useSpring, animated } from "react-spring";
 
 const RoundTomato = () => {
-  // const [start, setStart] = useState(false);
   return (
-    <Stage width={300} height={300}>
-      <Layer>
-        <Shape
-          sceneFunc={(context, shape) => {
-            context.beginPath();
-            context.arc(150, 150, 60, 0, 0.01 * Math.PI, false);
-            // shape.rotate(0);
-            context.fillStrokeShape(shape);
-            console.log("shape", shape);
-          }}
-          offsetX={0}
-          offsetY={0}
-          // rotation={360}
-          rotationDeg={180}
-          stroke="#EA5548"
-          strokeWidth={20}
-        />
-        {/* <Spring
-          native
-          form={{ value: 0 }}
-          to={{ value: 1.5 }}
-          config={{ duration: 3000 }}
-        >
-          {(props) => (
-            // console.log('props', props)
-            <animated.Shape
-              // sceneFunc={(context, shape) => {
-              //   context.beginPath();
-              //   context.arc(50, 50, 45, 0, props.value * Math.PI, false);
-              //   context.fill();
-              //   context.closePath();
-              //   context.fillStrokeShape(shape);
-              // }}
-              width={100}
-              height={100}
-              x={50}
-              y={50}
-              radius={45}
-              // innerRadius={40}
-              // outerRadius={70}
-              fill="#EA5548"
-              stroke="#fff"
-              strokeWidth={10}
-            />
-          )}
-        </Spring> */}
-      </Layer>
-    </Stage>
+    <Box
+      width="100px"
+      height="100px"
+      style={{ transform: "rotate(-90deg)" }}
+      display="flex"
+    >
+      <Stage width="100" height="100">
+        <Layer>
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.arc(50, 50, 40, 0 * Math.PI, 1.5 * Math.PI, false);
+              context.fill();
+              context.fillStrokeShape(shape);
+            }}
+            fill="#EA5548"
+            stroke="#EA5548"
+            strokeWidth={10}
+          />
+          {/* <Circle x={200} y={100} radius={50} fill="green" /> */}
+        </Layer>
+      </Stage>
+    </Box>
   );
 };
 
